d46b8f854ee3538ebdf4c221fd569ec5a64e5299
Andrei Tognolo <andreitognolo@gmail.com>
2014-04-07 22:35:16 -0300

    Init

---
+++ impl/js/main.js
@@ -0,0 +1,23 @@
+(function($){
+	$(window).hashchange(function() {
+		var hash = this.location.hash || '#';
+		hash = $.trim(hash);
+		if (!hash || hash == '#') {
+			this.location.hash = '#Main';
+			return;
+		}
+		hash = hash.substring(1);
+		var page = hash.split('?')[0];
+
+		$.ajax({
+		    url : 'pages/' + page + '.html',
+		    dataType : 'html',
+		    success : function(text) {
+		    	$('.content').html($(text).filter('section').html());
+		    }
+		});
+	});
+	
+	$(window).hashchange();
+	
+})(jQuery);
