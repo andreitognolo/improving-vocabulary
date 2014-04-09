bf70e97865d565348da50c16466e5e2f3d1cbf24
Andrei Tognolo <andreitognolo@gmail.com>
2014-04-09 19:08:13 -0300

    Init

--- impl/server.js
+++ impl/server.js
@@ -1,7 +1,11 @@
-var http = require("http"), url = require("url"), path = require("path"), fs = require("fs")
-port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-
+var http = require("http");
+var url = require("url");
+var path = require("path");
+var fs = require("fs");
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+var EpisodeService = require('./server/EpisodeService.js');
 
 function processStatic(uri, request, response) {
 	var filename = path.join(process.cwd(), uri);
@@ -36,17 +40,19 @@ function processStatic(uri, request, response) {
 	});
 }
 
-function processService(request, response) {
+function processService(uri, request, response) {
 	response.writeHead(200, {
 		"Content-Type" : "text/json"
 	});
-	response.end('19851118');
+	var service = uri.split('/')[2];
+	var func = uri.split('/')[3];
+	response.end(eval(service + "Service." + func + "()"));
 }
 
 http.createServer(function(request, response) {
 	var uri = url.parse(request.url).pathname;
 	if (uri.indexOf('/s/') == 0) {
-		processService(request, response);
+		processService(uri, request, response);
 	} else {
 		processStatic(uri, request, response);
 	}
---
+++ impl/server/EpisodeService.js
@@ -0,0 +1,3 @@
+exports.nextTranscription = function() {
+	return '19851118';
+}
