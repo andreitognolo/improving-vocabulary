b7320670dc4822e6daec3b7ac2ceedfbf8531add
Andrei Tognolo <andreitognolo@gmail.com>
2014-04-12 18:04:09 -0300

    MongoDB

--- impl/server/EpisodeService.js
+++ impl/server/EpisodeService.js
@@ -4,7 +4,9 @@ exports.nextTranscription = function() {
 			var MongoClient = require('mongodb').MongoClient
 			var format = require('util').format;
 
-			MongoClient.connect('mongodb://127.0.0.1:27017/improving-vocabulary', function(err, db) {
+			var mongoHost = process.env.OPENSHIFT_MONGODB_DB_HOST || '127.0.0.1';
+			var mongoIp = process.env.OPENSHIFT_MONGODB_DB_PORT || '27017';
+			MongoClient.connect('mongodb://' + mongoHost + ':' + mongoIp + '/improveyourvocabulary', function(err, db) {
 				if (err)
 					throw err;
 
