d6bfdc679a2ed3c38be7d430eabc8a4fa7c43884
Andrei Tognolo <andreitognolo@gmail.com>
2014-04-22 22:39:47 -0300

    Transcription

--- impl/js/message.js
+++ impl/js/message.js
@@ -3,12 +3,20 @@
 	$.success = function(text, time) {
 		var msg = $('<div class="alert alert-success">' + text + '</div>');
 		$('.message').append(msg);
-		msg.fadeOut(time || 5000);
+		
+		console.log('time', time || 15000);
+		
+		setTimeout(function() {
+			msg.fadeOut(1000);
+		}, time || 5000);
 	}
 	$.warning = function(text, time) {
 		var msg = $('<div class="alert alert-warning">' + text + '</div>');
 		$('.message').append(msg);
-		msg.fadeOut(time || 5000);
+		
+		setTimeout(function() {
+			msg.fadeOut(1000);
+		}, time || 5000);
 	}
 	
 })(jQuery);
