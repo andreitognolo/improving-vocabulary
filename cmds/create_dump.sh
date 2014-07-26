4ab4320d0d0e9f0aa44a9df7717b1512b4fe71d5
Andrei Tognolo <andreitognolo@gmail.com>
2014-07-26 15:00:42 -0300

    x

--- cmds/create_dump.sh
+++ cmds/create_dump.sh
@@ -1,2 +1,2 @@
 rhc port-forward -a improveyourvocabulary &&
-./mongodump -h 127.0.0.1:27017 -d improveyourvocabulary -u admin -p 4nVL27YyApru -o dump -v
+mongodump -h 127.0.0.1:27017 -d improveyourvocabulary -u admin -p 4nVL27YyApru -o dump -v
