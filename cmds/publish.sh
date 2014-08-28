95a6621831e2e0490bda9f2added62bd52a55a69
Andrei Tognolo <andreitognolo@gmail.com>
2014-08-27 23:43:11 -0300

    Ajustando scripts

--- cmds/publish.sh
+++ cmds/publish.sh
@@ -1,6 +1,5 @@
-cp -r impl/* rhc/improveyourvocabulary/
-cd rhc/improveyourvocabulary/
-git add .
-git commit -m "Publishing"
+cp -r ../* ../../rhc/improveyourvocabulary &&
+cd ../../rhc/improveyourvocabulary &&
+git add . &&
+git commit -m "Publishing" &&
 git push
-
