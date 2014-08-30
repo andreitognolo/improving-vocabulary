78b9d3ceb9fd0390d0234e1191504ea4bcd45baa
RenatoFerreiraSoares <renatoferreira656@gmail.com>
2014-08-30 19:17:19 -0300

    log colors

--- test/test-runner.js
+++ test/test-runner.js
@@ -3,30 +3,33 @@ var t = require( "qunitjs" );
 var tests = require( "./import-tests.js" );
 var MongoHelper = require(HOME + '/server/MongoHelper');
 
+var colors = require('colors');
+
+
 t.log(function( details ) {
     if ( !details.result ) {
-        var output = "TEST FAILED: " + details.name;
-        if ( details.actual ) {
-            output += "\nexpected: " + details.expected + ", actual: " + details.actual;
-        }
-        if ( details.source ) {
-            output += "\n" + details.source;
-        }
-        console.error( output );
+        var error = ("[TEST FAILED: " + details.module + " - " + details.name + " ] ").red;
+        console.error(error, "EXPECTED:", details.expected);
+        console.error(error, "ACTUAL:", details.actual);
+        console.error(error, "STACK:", details.source);
     }
 });
 
 t.done(function(result){
-    console.log("Failed test: " + result.failed);
-    console.log("Passed test: " + result.passed);
-    console.log("Total: " + result.total);
+    var info = "[TEST] PASSED:" + result.passed + ", FAILED:" + result.failed + ", TOTAL:" + result.total + ", TIME:" + result.runtime + " ms"; 
     if(result.failed){
+        console.log(info.red);
+        console.log("[TEST] BUILD FAILED".red);
         process.exit(1);
     }
+    console.log(info.green);
+    console.log("[TEST] BUILD SUCCESS".green);
 });
 
-t.testStart(function(){
+t.testStart(function(details){
     t.stop();
+    var testStart = ("[TEST STARTED: " + details.module + " / " + details.name + "]").yellow
+    console.log(testStart);
     MongoHelper.reset(function(){
         MongoHelper.init(function() {
             t.start();
@@ -34,12 +37,17 @@ t.testStart(function(){
     });
 });
 
-t.testDone(function(){
-   MongoHelper.db.close(); 
+t.testDone(function(details){
+    var res = "[TEST PASSED]".green;
+    if(details.failed){
+       res = "[TEST FAILED]".red;
+    }
+    console.log(res, ": ASSERTIONS:", details.total, ",FAILED:", details.failed, ",PASSED:", details.passed, "TIME: ", details.duration, " ms");
+    MongoHelper.db.close(); 
 });
 
 if (process.argv.length == 3) {
-	console.log('Executing test: ' + process.argv[2]);
+	console.log('[EXECUTING TEST]:'.green, process.argv[2]);
 	require(process.argv[2]).stack(t);
 } else {
 	var test = tests.import(t);
