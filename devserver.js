38b85320bce013a6cd0a2690fa51f5f75ddcc930
RenatoFerreiraSoares <renatoferreira656@gmail.com>
2014-09-14 09:25:09 -0300

    Server.js

---
+++ devserver.js
@@ -0,0 +1,54 @@
+function startServer(){
+    var path = require("path");
+    var fs = require("fs");
+    var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+    var ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+    function processStatic(resp) {
+        var filename = path.join(process.cwd(), resp.uri);
+        fs.exists(filename, function(exists) {
+            if (fs.statSync(filename).isDirectory())
+                filename = path.join(filename, 'index.html');
+
+            fs.readFile(filename, "binary", function(err, file) {
+                var contentType = resp.header(filename);
+                resp.sendSuccess(file, contentType);
+            });
+        });
+    }
+
+    function processService(resp) {
+        resp.onReceivedData(function(body){
+            var service = require("./server/" + resp.urlArg(2) + "Service");
+            var func = service[resp.urlArg(3)];
+
+            if(body){
+                body = JSON.parse(body);
+            }
+
+            var ret = func.apply(service, [ body ]);
+            ret.done(function(result){
+                result = JSON.stringify(result);
+                resp.sendSuccess(result || 'void', 'text/json');
+            });
+        });
+    } 
+
+    var serv = require('./server/Server').server();
+    serv.action('/s', processService);
+    serv.action('/', processStatic);
+    serv.listen(parseInt(port, 10), ip_address);
+}
+
+require('./server/MongoHelper').init(function() {
+
+    var mongoHelper = require('./server/MongoHelper');
+    if (mongoHelper.db) {
+        console.log('database is not undefined');
+    } else {
+        console.log('ixi... it is undefined');
+    }
+
+    startServer();
+});
+
--- server/Server.js
+++ server/Server.js
@@ -1,7 +1,7 @@
 exports = (function (exports){
     var http = require("http");
     var url = require("url");
-    
+
     function Result(uri, req, resp){
         this.req = req;
         this.resp = resp;
@@ -12,15 +12,15 @@ exports = (function (exports){
         this.resp.statusCode = errorCode;
         this.resp.setHeader("Content-Type", "text/plain");
         this.resp.write(error);
-		this.resp.end();
+        this.resp.end();
     }    
-    
+
     Result.prototype.header = function(filename){
-         if(filename.indexOf('.html') >= 0){
+        if(filename.indexOf('.html') >= 0){
             return "text/html";   
         }
     }   
-    
+
     Result.prototype.sendSuccess = function(body, contentType){
         this.resp.statusCode = 200;
         if(contentType){
@@ -29,27 +29,27 @@ exports = (function (exports){
         this.resp.write(body, "binary");
         this.resp.end();
     }
-    
+
     Result.prototype.urlArg = function(index){
         return this.uri.split('/')[index];
     }
-    
+
     Result.prototype.onReceivedData = function(cb){
         var body = "";
         if(this.req.method === "POST"){
-              this.req.on('data', function(data){
+            this.req.on('data', function(data){
                 body+=data;
-              });
+            });
 
-              this.req.on('end', function(){
-                 cb(body); 
-              });
+            this.req.on('end', function(){
+                cb(body); 
+            });
         } else if(this.req.method === "GET"){
-              var params = require('./util/querystring').params(this.req.url);
-              if (params.data) {
-                  body = params.data[0];
-              }
-              cb(body); 
+            var params = require('./util/querystring').params(this.req.url);
+            if (params.data) {
+                body = params.data[0];
+            }
+            cb(body); 
         }
     }
 
@@ -71,23 +71,23 @@ exports = (function (exports){
         if(!action){
             return _findActionByPath(this.actions, path);
         }
-        
+
         this.actions.push({
             path: path,
             action: action
         });
-        
+
         function _sort(a, b){
-          if (a.path < b.path)
-             return -1;
-          if (a.path > b.path)
-            return 1;
-          return 0;
+            if (a.path < b.path)
+                return -1;
+            if (a.path > b.path)
+                return 1;
+            return 0;
         }
-        
+
         this.actions = this.actions.sort(_sort).reverse();
     }
-    
+
 
     Server.prototype.filter = function(filt){
         this.filters.push(filt);
@@ -99,22 +99,52 @@ exports = (function (exports){
         console.log("http://localhost:", port);
         console.log("CTRL + C to Shutdown");
     }
-    
+
+    function _startsWith(uri, starts){
+        function _clean(arr){
+            arr = arr.filter(function(el){
+                if(el){
+                    return true;
+                }
+                return false;
+            })
+            return arr;
+        }
+
+        uri = uri.split('/');
+        starts = starts.split('/');
+        uri = _clean(uri);
+        starts = _clean(starts);
+
+        if(uri.length < starts.length){
+            return false;
+        }
+
+        for(var i=0; i<starts.length; i++){
+            if(starts[i] != uri[i]){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
     function _findActionByPath(list, path){
 
         for(var i=0; i < list.length; i++){
             var obj = list[i];
-            if(path.indexOf(obj.path) === 0){
+            if(_startsWith(path, obj.path)){
                 return obj.action;
             }
         }
 
         throw "Ops, no action found";
     }
-    
+
     exports.server = function(){
         return new Server();   
     }
-    
+
     return exports;
 })(exports);
