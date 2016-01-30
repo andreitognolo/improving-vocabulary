16aa4136c6d1ee4f4b6f663e870564cd2f573c3d
Andrei Tognolo <andreitognolo@gmail.com>
2016-01-30 12:58:44 -0300

    Remove user and password

--- server/MongoHelper.js
+++ server/MongoHelper.js
@@ -8,19 +8,21 @@ function init(cb) {
 }
 
 function connect(cb) {
-	var MongoClient = require('mongodb').MongoClient
-	var mongoHost = process.env.OPENSHIFT_MONGODB_DB_HOST || '127.0.0.1';
-	var mongoIp = process.env.OPENSHIFT_MONGODB_DB_PORT || '27017';
-	var mongoUser = 'admin';
-	var mongoPassword = '4nVL27YyApru';
-	var mongoURL = 'mongodb://' + mongoUser + ':' + mongoPassword + '@' + mongoHost + ':' + mongoIp + '/improveyourvocabulary';
-	
-	MongoClient.connect(mongoURL, function(err, db) {
-		if (err)
-			throw err;
-		
-		cb(db);
-	});
+    var MongoClient = require('mongodb').MongoClient
+    var mongoHost = process.env.OPENSHIFT_MONGODB_DB_HOST || '127.0.0.1';
+    var mongoIp = process.env.OPENSHIFT_MONGODB_DB_PORT || '27017';
+    var mongoUser = 'admin';
+    var mongoPassword = '4nVL27YyApru';
+    var mongoURL = 'mongodb://' + mongoHost + ':' + mongoIp + '/improveyourvocabulary';
+
+    console.log('Connecting to: ', mongoURL);
+
+    MongoClient.connect(mongoURL, function (err, db) {
+        if (err)
+            throw err;
+
+        cb(db);
+    });
 }
 
 function reset(cb) {
